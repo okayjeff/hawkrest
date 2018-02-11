d76858358747300074ffac4c61aa3ad994d5f5e9
Jeff G <jg@okayjeff.com>
2018-02-11 10:23:45 -0500

    Add test skeletons.

---
+++ tests/test_command.py
@@ -0,0 +1,17 @@
+import mock
+
+from tests.base import BaseTest
+
+
+class TestManagementCommand(BaseTest):
+    def test_error_raised_if_requests_not_imported(self):
+        pass
+
+    def test_error_raised_if_url_not_specified(self):
+        pass
+
+    def test_error_raised_if_creds_missing(self):
+        pass
+
+    def test_response_unverified_without_auth_header(self):
+        pass
